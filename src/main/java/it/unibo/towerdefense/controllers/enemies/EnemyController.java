b05fe14ae45e36790ee85f028209428d539aac76
Davide <davide.fiocchi2@studio.unibo.it>
2024-02-23 11:19:02 +0100

    Added some interfaces

---
+++ src/main/java/it/unibo/towerdefense/controllers/enemies/EnemyController.java
@@ -0,0 +1,9 @@
+package it.unibo.towerdefense.controllers.enemies;
+
+public interface EnemyController {
+
+    public void update();
+
+    public void render();
+
+}
---
+++ src/main/java/it/unibo/towerdefense/views/enemies/EnemiesRenderer.java
@@ -0,0 +1,13 @@
+package it.unibo.towerdefense.views.enemies;
+
+import it.unibo.towerdefense.views.Renderer;
+
+/**
+ * Renders enemies on the screen.
+ */
+public interface EnemiesRenderer extends Renderer{
+    /**
+     * Renders enemies on the screen.
+     */
+    void render();
+}
