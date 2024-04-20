d467256157b0d0d92361911b1f85b145c943d784
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2024-04-20 19:58:12 +0200

    Fixed checkstyle problems for strategyFactory tests

--- src/test/java/it/unibo/towerdefense/models/defenses/TestEnemyChoiceStrategyFactoryImpl.java
+++ src/test/java/it/unibo/towerdefense/models/defenses/TestEnemyChoiceStrategyFactoryImpl.java
@@ -8,9 +8,12 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+
 
 /**
  * Test class for the CollisionBoxImpl class.
@@ -25,10 +28,10 @@ class TestEnemyChoiceStrategyFactoryImpl {
     private static final int TEST_HP = 100;
 
     private List<Pair<LogicalPosition, Integer>> testTargets;
-
+    private Iterator<LogicalPosition> testPositions;
     /**Setup the enemy list. */
     @BeforeEach
-    void setUp(){
+    void setUp() {
         testTargets = new LinkedList<>();
     }
 
@@ -37,29 +40,41 @@ class TestEnemyChoiceStrategyFactoryImpl {
      */
     @Test
     void testClosestTargetStrategy() {
+        /**create positions for this test.*/
+        final LogicalPosition testPos1 = new LogicalPosition(5, 5);
+        final LogicalPosition testPos2 = new LogicalPosition(2000, 2000);
+        final LogicalPosition testPos3 = new LogicalPosition(-2000, -2000);
+        final LogicalPosition testPos4 = new LogicalPosition(1, 1);
+        final LogicalPosition testPos5 = new LogicalPosition(2, 2);
+        final LogicalPosition testPos6 = new LogicalPosition(7, 7);
+        final LogicalPosition testPos7 = new LogicalPosition(4, 4);
+        final LogicalPosition testPos8 = new LogicalPosition(4, 4);
+        /**create expected results */
+        final Map<Integer, Integer> expectedResultTest1 = new HashMap<>();
+        final Map<Integer, Integer> expectedResultTest2 = Map.of(0, TEST_DAMAGE);
+        final Map<Integer, Integer> expectedResultTest3 = Map.of(0, TEST_DAMAGE);
+        final Map<Integer, Integer> expectedResultTest4 = 
+        Map.of(0, TEST_DAMAGE, 3, TEST_DAMAGE, 4, TEST_DAMAGE, 6, TEST_DAMAGE, 7, TEST_DAMAGE);
+
+        testPositions = List.of(testPos1, testPos2, testPos3, testPos4, testPos5,
+        testPos6, testPos7, testPos8).iterator();    
         EnemyChoiceStrategy strategy = factory.closestTargets(TEST_MAX_TARGETS, TEST_RANGE, TEST_POSITION);
         /**Test 1: no target possible*/
-        Assertions.assertEquals(strategy.execute(List.of(), TEST_DAMAGE), Map.of());
-
-        /**Test 2: 1 target */
-        
-        testTargets.add(new ImmutablePair<>(new LogicalPosition(5, 5),TEST_HP));
-        Assertions.assertEquals(strategy.execute(testTargets, TEST_DAMAGE), Map.of(0,TEST_DAMAGE));
-
-        /**Add unreachable targets, expect same result */
-        testTargets.add(new ImmutablePair<>(new LogicalPosition(20000, -20000),TEST_HP));
-        testTargets.add(new ImmutablePair<>(new LogicalPosition(-20000, 20000),TEST_HP));
-        Assertions.assertEquals(strategy.execute(testTargets, TEST_DAMAGE), Map.of(0,TEST_DAMAGE));
-
-        /**Add 5 reachable targets,expect closest 5 */
-        testTargets.add(new ImmutablePair<>(new LogicalPosition(1, 1), TEST_HP));
-        testTargets.add(new ImmutablePair<>(new LogicalPosition(2, 2), TEST_HP));
+        Assertions.assertEquals(strategy.execute(List.of(), TEST_DAMAGE), expectedResultTest1);
+        /**Test 2: 1 target*/
+        testTargets.add(new ImmutablePair<>(testPositions.next(), TEST_HP));
+        Assertions.assertEquals(strategy.execute(testTargets, TEST_DAMAGE), expectedResultTest2);
+        /**Test 3:Add unreachable targets, expect same result */
+        testTargets.add(new ImmutablePair<>(testPositions.next(), TEST_HP));
+        testTargets.add(new ImmutablePair<>(testPositions.next(), TEST_HP));
+        Assertions.assertEquals(strategy.execute(testTargets, TEST_DAMAGE), expectedResultTest3);
+        /**Test 4:Add 5 reachable targets,expect closest 5 */
+        testTargets.add(new ImmutablePair<>(testPositions.next(), TEST_HP));
+        testTargets.add(new ImmutablePair<>(testPositions.next(), TEST_HP));
         /**Index 5:expected to not be targeted*/
-        testTargets.add(new ImmutablePair<>(new LogicalPosition(7, 7), TEST_HP)); 
-        testTargets.add(new ImmutablePair<>(new LogicalPosition(4, -4), TEST_HP));
-        testTargets.add(new ImmutablePair<>(new LogicalPosition(3, -3), TEST_HP));
-        Assertions.assertEquals(strategy.execute(testTargets, TEST_DAMAGE),
-        Map.of(0, TEST_DAMAGE, 3, TEST_DAMAGE, 4, TEST_DAMAGE, 6,TEST_DAMAGE, 7, TEST_DAMAGE)
-        );
+        testTargets.add(new ImmutablePair<>(testPositions.next(), TEST_HP)); 
+        testTargets.add(new ImmutablePair<>(testPositions.next(), TEST_HP));
+        testTargets.add(new ImmutablePair<>(testPositions.next(), TEST_HP));
+        Assertions.assertEquals(strategy.execute(testTargets, TEST_DAMAGE), expectedResultTest4);
     }
 }
