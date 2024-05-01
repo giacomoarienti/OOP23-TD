4711c6b2c3b65cb52981c45ac06be01796a685ff
Davide Fiocchi <davide.fiocchi2@studio.unibo.it>
2024-05-01 21:16:07 +0200

    Collapsed WavesManager into Enemies

--- src/main/java/it/unibo/towerdefense/models/enemies/EnemiesImpl.java
+++ src/main/java/it/unibo/towerdefense/models/enemies/EnemiesImpl.java
@@ -1,6 +1,7 @@
 package it.unibo.towerdefense.models.enemies;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import it.unibo.towerdefense.controllers.enemies.EnemyInfo;
@@ -10,33 +11,52 @@ import it.unibo.towerdefense.controllers.map.MapController;
 public class EnemiesImpl implements Enemies {
 
     private final String waveConfigFile = "it/unibo/towerdefense/models/enemies/waves.config";
-    private final String typesConfigFile = "it/unibo/towerdefense/models/enemies/types.config";
-    private final EnemyCollection collection;
-    private final WavesManager waves;
+    private final String typesConfigFile = "it/unibo/towerdefense/models/enemies/types.json";
+    private final EnemyCollection enemies;
+    private final EnemySpawner spawner;
+    private final WaveSupplier supplier;
+    private final GameController gc;
+    private Optional<Wave> current = Optional.empty();
 
     public EnemiesImpl(final MapController map, final GameController gc){
-        this.collection = new EnemyCollectionImpl(map);
-        this.waves = new SimpleWavesManager(collection, gc, map.getSpawnPosition(), new WavePolicySupplierImpl(waveConfigFile), new ConfigurableEnemyCatalogue(typesConfigFile));
+        this.enemies = new EnemyCollectionImpl(map);
+        this.spawner = new SimpleEnemySpawner(map.getSpawnPosition(), enemies);
+        this.supplier = new PredicateBasedRandomWaveGenerator(new WavePolicySupplierImpl(waveConfigFile), new ConfigurableEnemyCatalogue(typesConfigFile));
+        this.gc = gc;
     }
 
     @Override
     public void update() {
-        collection.move();
-        waves.update();
+        enemies.move();
+        if(current.isPresent()){
+            if(current.get().hasNext()){
+                current.get().next().ifPresent(et -> spawner.spawn(et));
+            }else{
+                current = Optional.empty();
+            }
+        }else{
+            if(enemies.areDead()){
+                gc.advanceWave();
+            }
+        }
     }
 
     @Override
     public Set<Enemy> getEnemies() {
-        return collection.getEnemies();
+        return enemies.getEnemies();
     }
 
     @Override
     public List<EnemyInfo> getEnemiesInfo() {
-        return collection.getEnemiesInfo();
+        return enemies.getEnemiesInfo();
     }
 
     @Override
     public void spawn(int wave) {
-        waves.spawn(wave);
+        if(current.isPresent()){
+            throw new IllegalStateException("A wave is already being spawned.");
+        }else{
+            current = Optional.of(supplier.apply(wave));
+        }
     }
 }
