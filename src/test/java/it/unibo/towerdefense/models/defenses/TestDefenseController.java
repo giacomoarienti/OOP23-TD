9a6d6748cf41f8a7c63b79141393c6300ea96609
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2024-05-05 21:44:10 +0200

    Created file for controller test.

---
+++ src/test/java/it/unibo/towerdefense/models/defenses/TestDefenseController.java
@@ -0,0 +1,42 @@
+package it.unibo.towerdefense.models.defenses;
+
+import org.junit.jupiter.api.Test;
+
+/**class for testing defense controller.*/
+public class TestDefenseController {
+    @Test
+    /**Test for "toJson" method.*/
+    public void testToJson() {
+
+    }
+
+    @Test
+    /**Test for "attackEnemies" method.*/
+    public void testAttackEnemies() {
+        
+    }
+
+    @Test
+    /**Test for "getBuildables" method.*/
+    public void testGetBuildables() {
+        
+    }
+
+    @Test
+    /**Test for "disassembleDefense" method.*/
+    public void testDisassembleDefense() {
+        
+    }
+
+    @Test
+    /**Test for "toJson" method.*/
+    public void testRender() {
+        
+    }
+
+    @Test
+    /**Test for "fromJson".*/
+    public void testFromJson() {
+
+    }
+}
