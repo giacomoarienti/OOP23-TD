ae91424fdfa043fb7e0fceac22acab3719e3ea2c
giacomoarienti <arientigiacomo2003@gmail.com>
2024-05-12 17:05:55 +0200

    SavingsViewImpl implementation

--- src/main/java/it/unibo/towerdefense/views/savings/SavingsViewImpl.java
+++ src/main/java/it/unibo/towerdefense/views/savings/SavingsViewImpl.java
@@ -3,6 +3,9 @@ package it.unibo.towerdefense.views.savings;
 import java.util.Collections;
 import java.util.List;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import it.unibo.towerdefense.controllers.savings.SavingsController;
@@ -40,7 +43,41 @@ public class SavingsViewImpl implements SavingsView {
      */
     @Override
     public JPanel build(final Runnable onClose) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'build'");
+        // create main panel
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        // create a panel for each saving
+        for (final Saving saving: this.savings) {
+            mainPanel.add(
+                new SavingsPanel(
+                    saving,
+                    onClose
+                )
+            );
+        }
+        // return the main panel
+        return mainPanel;
+    }
+
+    private class SavingsPanel extends JPanel {
+        private static final long serialVersionUID = 1L;
+        private final Saving saving;
+        private final Runnable onClose;
+
+        private void onClick() {
+            controller.loadSaving(this.saving);
+            this.onClose.run();
+        }
+
+        public SavingsPanel(final Saving saving, final Runnable onClose) {
+            this.saving = saving;
+            this.onClose = onClose;
+            // build the view
+            this.add(new JLabel(saving.getName()));
+            // create load button and add it to the panel
+            final JButton loadButton = new JButton("Load");
+            loadButton.addActionListener(e -> onClick());
+            this.add(loadButton);
+        }
     }
 }
