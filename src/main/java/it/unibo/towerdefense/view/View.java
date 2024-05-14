0d26e1c444cd8ddcbfcf29f4ca448df2912e89b1
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2024-05-14 17:25:26 +0200

    View java-doc

--- src/main/java/it/unibo/towerdefense/view/View.java
+++ src/main/java/it/unibo/towerdefense/view/View.java
@@ -2,23 +2,50 @@ package it.unibo.towerdefense.view;
 
 import it.unibo.towerdefense.commons.engine.Size;
 import it.unibo.towerdefense.controller.gamelauncher.GameLauncherController;
-import it.unibo.towerdefense.controller.menu.MenuController;
+import it.unibo.towerdefense.controller.menu.StartMenuController;
 import it.unibo.towerdefense.controller.savings.SavingsController;
 import it.unibo.towerdefense.view.graphics.GameRenderer;
 
 public interface View {
 
+    /**
+     * Display the GameLauncherView.
+     * @param controller the Controller of the GameLauncherView
+     */
     void displayLauncher(GameLauncherController controller);
 
-    void displayGame(Size size);
+    /**
+     * Display the main game window.
+     * @param size the size of the window
+     */
+    void displayWindow(Size size);
 
-    void displayStartMenu(MenuController menuController);
+    /**
+     * Display the StartMenu.
+     * @param menuController the Controller of the StartMenuView
+     */
+    void displayStartMenu(StartMenuController menuController);
 
+    /**
+     * Display the saving selection view.
+     * @param savingsController the Controller of the SavingsView
+     */
     void displaySavings(SavingsController savingsController);
 
+    /**
+     * Close the window if present.
+     */
     void close();
 
+    /**
+     * Creates an instance of the GameRenderer.
+     * @param size the size of the renderer
+     */
     void createGameRenderer(Size size);
 
+    /**
+     * Get the GameRenderer.
+     * @return the GameRenderer
+     */
     GameRenderer getGameRenderer();
 }
--- src/main/java/it/unibo/towerdefense/view/ViewImpl.java
+++ src/main/java/it/unibo/towerdefense/view/ViewImpl.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 import it.unibo.towerdefense.commons.engine.Size;
 import it.unibo.towerdefense.controller.gamelauncher.GameLauncherController;
-import it.unibo.towerdefense.controller.menu.MenuController;
+import it.unibo.towerdefense.controller.menu.StartMenuController;
 import it.unibo.towerdefense.controller.savings.SavingsController;
 import it.unibo.towerdefense.view.gamelauncher.GameLauncherViewImpl;
 import it.unibo.towerdefense.view.graphics.GameRenderer;
@@ -14,17 +14,23 @@ import it.unibo.towerdefense.view.savings.SavingsViewImpl;
 import it.unibo.towerdefense.view.window.Window;
 import it.unibo.towerdefense.view.window.WindowImpl;
 
+/**
+ * Implementation of the View interface.
+ */
 public class ViewImpl implements View {
 
     private Window window;
     private GameRenderer gameRenderer;
 
     /**
-     * Constructor for the ViewImpl class.
+     * Empty constructor.
      */
     public ViewImpl() {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void displayLauncher(final GameLauncherController controller) {
         // instantiate the game launcher view
@@ -32,34 +38,61 @@ public class ViewImpl implements View {
         gameLauncherView.display();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void displayGame(final Size size) {
+    public void displayWindow(final Size size) {
         this.window = new WindowImpl(size);
         this.window.display();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void displaySavings(final SavingsController controller) {
+        if (Objects.isNull(this.window)) {
+            throw new IllegalStateException("Window not created yet");
+        }
         final var savingsView = new SavingsViewImpl(controller);
         this.window.displayModal("Savings", savingsView);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void displayStartMenu(final MenuController controller) {
+    public void displayStartMenu(final StartMenuController controller) {
+        if (Objects.isNull(this.window)) {
+            throw new IllegalStateException("Window not created yet");
+        }
         final var startMenu = new StartMenuViewImpl(controller);
         this.window.displayModal("Start Menu", startMenu);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void close() {
+        if (Objects.isNull(this.window)) {
+            throw new IllegalStateException("Window not created yet");
+        }
         this.window.close();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void createGameRenderer(final Size size) {
         this.gameRenderer = new GameRendererImpl(size, this.window);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public GameRenderer getGameRenderer() {
         if (Objects.isNull(gameRenderer)) {
