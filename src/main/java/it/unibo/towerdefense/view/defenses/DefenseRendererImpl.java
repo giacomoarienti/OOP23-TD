0edfde897ed27c45d7bd1afcdc91202878f3b670
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2024-05-15 10:44:52 +0200

    DefenseManager refactor.

---
+++ src/main/java/it/unibo/towerdefense/view/defenses/DefenseRendererImpl.java
@@ -0,0 +1,13 @@
+package it.unibo.towerdefense.view.defenses;
+
+import java.util.stream.Stream;
+
+import it.unibo.towerdefense.commons.dtos.defenses.DefenseDescription;
+
+public class DefenseRendererImpl implements DefenseRenderer {
+
+    @Override
+    public void render(Stream<DefenseDescription> defenses) {
+
+    }
+}
