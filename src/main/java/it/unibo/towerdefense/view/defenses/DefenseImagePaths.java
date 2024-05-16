8385baf2d8681c79be442eb374f60e0c627cd86e
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2024-05-16 13:26:32 +0200

    Added static class for creating images file path for defenses.

---
+++ src/main/java/it/unibo/towerdefense/view/defenses/DefenseImagePaths.java
@@ -0,0 +1,30 @@
+package it.unibo.towerdefense.view.defenses;
+
+import java.util.Map;
+import it.unibo.towerdefense.model.defenses.DefenseType;
+
+/**creates paths for the images of defenses.*/
+public final class DefenseImagePaths {
+
+    private static final String BASE_PATH = "src/main/java/it/unibo/towerdefense/view/defenses";
+    private static Map<DefenseType, String> ImagesBaseNames =Map.of
+    (DefenseType.ARCHERTOWER, "Defense",
+    DefenseType.BOMBTOWER, "Bomb",
+    DefenseType.WIZARDTOWER, "Wizard",
+    DefenseType.THUNDERINVOKER, "Thunder"
+    );
+
+    /**
+     * @param type type of defense.
+     * @param level level of defense.
+     * @return path of the image for a given defense of type x level y.
+     */
+    public static String buildPath(DefenseType type, int level) {
+        return BASE_PATH + ImagesBaseNames.get(type) + level;
+    }
+
+    /**Private constructor.*/
+    private DefenseImagePaths() {
+
+    }
+}
